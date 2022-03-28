e6528ec2e1da1aba5f42ea25f59aa150b2c99f3b
Yusong Shi <jackshi.sys@gmail.com>
2022-03-28 12:17:55 -0400

    express cors, morgan

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,28 @@
 const express = require('express')
-const cors = require()
-
+const cors = require('cors')
+const logger = require('morgan')
+const db = require('./db')
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
 
+
+
+
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.send('This is root!')
+})
+
+
+
+
+
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT} 🫠`)
 })
 
-app.get('/', (req, res) => {
-    res.send("You're a wizard, Harry!")
-  })
