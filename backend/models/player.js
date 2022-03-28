d18c2fda4eedbae14dbe9c942b95fade01742a4e
Yusong Shi <jackshi.sys@gmail.com>
2022-03-28 14:05:21 -0400

    player schema

--- backend/models/player.js
+++ backend/models/player.js
@@ -1,7 +1,17 @@
 const { Schema } = require('mongoose')
 
 const Player = new Schema(
-    {},
+    {
+        name: {type: string, required: true },
+        position:{type: string, required: true},
+        points: {type: number, required: true},
+        rebounds: {type: number, required: true},
+        assists: {type: number, required: true},
+        championships: {type: number, required: true},
+        bioHighlight:{type: string, required: true},
+        biography: {type: string, required: true},
+        image: {type: string, required: true}
+    },
     {timestamps: true}
 )
 
