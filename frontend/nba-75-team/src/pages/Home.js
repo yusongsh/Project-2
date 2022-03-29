5278fd8a4bb15cac9f74fb96276a2cf2a2591da1
Yusong Shi <jackshi.sys@gmail.com>
2022-03-29 17:43:29 -0400

    main section, hero working

--- frontend/nba-75-team/src/pages/Home.js
+++ frontend/nba-75-team/src/pages/Home.js
@@ -1,8 +1,69 @@
 import React from 'react'
+import './style/home.css'
+import Search from '../components/Search'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='Home'>
+      <div className='hero'>
+        <div className='text'>
+          <h5 className='Subtitle'>SEARCH YOUR FAVORITE PLAYER</h5>
+          <h1 className='Title'>NBA 75TH ANNIVERSARY</h1>
+        </div>
+        <div>
+          <Search />
+        </div>
+      </div>
+
+    <div className='intro-wrapper'>
+      <div className='intro-content'>
+        <div className='sm-title'>
+          <h3 >75TH ANNIVERSARY TEAM </h3>
+        </div>
+        <div className='paragraph'>
+          <p>The NBA 75th Anniversary Team, also referred to as the NBA 75, was chosen in 2021 to honor the 75th anniversary of the founding of the National Basketball Association (NBA). It was the fourth and most recent anniversary team in the league. Similar to the 50 Greatest Players in NBA History in 1996, a panel of media members, current and former players, coaches, general managers, and team executives selected the greatest players in league history. Tasked with compiling a list of 75 players, the committee named an additional 76th member due to a tie in voting. The list is part of the league's anniversary celebration during the 2021–22 season. Forty-five of the seventy-six players were later assembled in Cleveland, during the halftime ceremony of the 2022 All-Star Game.</p>
+        </div>
+      </div>
+    </div>
+
+    <div className='card-wrapper'>
+      <div className='cards'>
+        <div className='card'>
+          <div>
+            {/* <img ref={`./img/lbj.jpeg`} alt=''></img> */}
+          </div>
+          <div>
+            <h5>LEBRON JAMES</h5>
+          </div>
+          <div>
+            <h3>RARE COMBINATION OF SIZE, SKILL AND SAVVY.</h3>
+          </div>
+          
+        </div>
+        <div className='card'>
+    
+        </div>
+        <div className='card'>
+          
+        </div>
+        <div className='card'>
+          
+        </div>
+        <div className='card'>
+          
+        </div>
+        <div className='card'>
+          
+        </div>
+
+      </div>
+    </div>
+
+
+
+
+
+      </div>
   )
 }
 
