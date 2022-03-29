dd56bb59c8aba08dbdfe587a4cdad2c4aaf61f26
Yusong Shi <jackshi.sys@gmail.com>
2022-03-29 10:54:19 -0400

    React app created

---
+++ frontend/nba-75-team/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import Nav from './components/Nav'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>WHat up</h1>
+    </div>
+  );
+}
+
+export default App;
