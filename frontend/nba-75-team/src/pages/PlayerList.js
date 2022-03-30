958db31dbb74c16b0ecce01e66ec35ea51ddb12e
Yusong Shi <jackshi.sys@gmail.com>
2022-03-29 22:16:35 -0400

    Playerlist page set up

--- frontend/nba-75-team/src/pages/PlayerList.js
+++ frontend/nba-75-team/src/pages/PlayerList.js
@@ -1,9 +1,17 @@
-
+import './style/playerList.css'
 import React from 'react'
+
 
 function PlayerList() {
   return (
-    <div>PlayerList</div>
+    <div className='PlayerList-wrapper'>
+      <div className='playerlist-content'>
+          <div className='playerlist-image card-8'></div>
+            <p className='playlist-highlight'>Selected for being pioneers that have helped shape, define, and redefine the game. This is the NBA 75th Anniversary Team.</p>
+        </div>
+    </div>
+
+  
   )
 }
 
