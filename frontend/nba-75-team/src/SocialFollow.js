ea3e5e516492f5b5e7954e6bc2f9541b74d331a7
Yusong Shi <jackshi.sys@gmail.com>
2022-03-30 23:25:11 -0400

    more media query changes

--- frontend/nba-75-team/src/SocialFollow.js
+++ frontend/nba-75-team/src/SocialFollow.js
@@ -5,22 +5,22 @@ import {
     faGithub,
     faReact,
   } from "@fortawesome/free-brands-svg-icons";
-
+import './App.css'
 
 export default function SocialFollow() {
   return (
     <div className="social-container">
       <a href="https://www.linkedin.com/in/yusong-sh/" rel="noreferrer" target="_blank"
         className="Linked-In" >
-        <FontAwesomeIcon icon={faLinkedin} size="1x" />
+        <FontAwesomeIcon className="icon" icon={faLinkedin} size="1x" />
       </a>
       <a href="https://github.com/yusongsh" rel="noreferrer" target="_blank"
         className="GitHub" >
-        <FontAwesomeIcon icon={faGithub} size="1x" />
+        <FontAwesomeIcon className="icon" icon={faGithub} size="1x" />
       </a>
       <a href="https://yusong.space" rel="noreferrer" target="_blank"
         className="Porfolio" >
-        <FontAwesomeIcon icon={faReact} size="1x" />
+        <FontAwesomeIcon className="icon" icon={faReact} size="1x" />
       </a>
 </div>
   );
